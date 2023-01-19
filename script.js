a455320824af64d8953a177b97c73a5d9e3e50c2
shubh-s-lab <123099409+shubh-s-lab@users.noreply.github.com>
2023-01-19 21:33:56 +0530

    Tent
    Tent

---
+++ script.js
@@ -0,0 +1,35 @@
+function calc() {
+    let o = document.getElementById('ot').value;
+    let i = document.getElementById('in').value;
+    let h = document.getElementById('ht').value;
+
+    let diff = (o - i) / 2;
+    if (diff != 0) {
+        let ad = diff * diff + h * h;
+        let param = diff;
+        let ans = [];
+        let temp;
+        while (param >= 3) {
+            temp = param * param + ad;
+            ans.push(Math.sqrt(temp));
+            param -= 3;
+        }
+        ans.push(Math.sqrt(ad));
+        for (let k = 0; k < ans.length; ++k) {
+            let x = ans[k];
+            let intPart = Math.floor(x);
+            let fracPart = x - intPart;
+            let nw = fracPart * 12;
+            let nwinp, nwfp;
+            nwinp = Math.floor(nw);
+            nwfp = nw - nwinp;
+            if (nwfp >= 0.5) {
+                document.write(intPart + " " + nwinp + " <br>");
+            } else {
+                nwinp = nwinp + 1;
+                document.write(intPart + " " + nwinp + " <br>");
+            }
+        }
+    }
+
+}
